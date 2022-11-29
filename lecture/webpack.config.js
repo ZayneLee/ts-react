0c129b3dace67ce9362307de616e456b4c965610
Zayne <star1582@naver.com>
2022-11-29 23:03:04 +0900

    기본 세팅

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development', // production
-    devtool: 'eval', // hidden-source-map
-    resolve: {
-        extensions: ['.jsx', '.js', '.tsx', '.ts'],
-    },
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".jsx", ".js", ".tsx", ".ts"],
+  },
 
-    entry: {
-        app: './client'
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-        }]
-    },
-    plugins: [],
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist',
-    }
-}
+  entry: {
+    app: "./client",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+};
